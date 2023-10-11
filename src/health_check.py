b68b5013a9323349180b6d319cd88b01c4cf2fae
Milad Natanzi <miladnatanzi@gmail.com>
2023-10-11 18:27:01 -0400

    Update health_check.py

--- src/health_check.py
+++ src/health_check.py
@@ -1,30 +1,34 @@
 # health_check.py
-import ricxappframe.rmr as rmr
+from ricxappframe.xapp_frame import RMRXapp
 import logging
 
-def perform_health_check(mrc):
-    """
-    Function to perform RMR Health Check.
-    This function will send a health check message to the RMR and wait for a response.
-    You might need to modify this function based on the actual health check requirements.
-    """
-    # Create RMR message for health check
-    health_check_msg = rmr.rmr_alloc_msg(mrc, 1024)  # replace 1024 with the actual size needed
-    health_check_msg.contents.mtype = 100  # replace 100 with the actual message type for health checks
-    health_check_msg.contents.len = 0  # assuming no payload for health check; change if there's a payload
+class HealthCheckXapp(RMRXapp):
+    def __init__(self):
+        super().__init__()
 
-    # Send RMR message
-    health_check_msg = rmr.rmr_send_msg(mrc, health_check_msg)
-    if health_check_msg.contents.state != 0:
-        logging.error("Failed to send RMR health check message")
-        return False
+    def rmr_health_check(self):
+        """
+        Method to perform RMR Health Check.
+        This method will send a health check message to the RMR and wait for a response.
+        You might need to modify this method based on the actual health check requirements.
+        """
+        # Create RMR message for health check
+        health_check_msg = self.rmr_alloc_msg(1024)  # replace 1024 with the actual size needed
+        health_check_msg.mtype = 100  # replace 100 with the actual message type for health checks
+        health_check_msg.len = 0  # assuming no payload for health check; change if there's a payload
 
-    # Receive RMR response
-    response_msg = rmr.rmr_rcv_msg(mrc, health_check_msg)
-    if response_msg.contents.state != 0 or response_msg.contents.mtype != 200:  # replace 200 with the actual response message type
-        logging.error("Failed to receive RMR health check response")
-        return False
+        # Send RMR message
+        health_check_msg = self.rmr_send_msg(health_check_msg)
+        if health_check_msg.state != 0:
+            logging.error("Failed to send RMR health check message")
+            return False
 
-    # If we reach here, the health check was successful
-    logging.info("RMR health check successful")
-    return True
+        # Receive RMR response
+        response_msg = self.rmr_rcv_msg(timeout=2)  # timeout in seconds, adjust as needed
+        if response_msg.state != 0 or response_msg.mtype != 200:  # replace 200 with the actual response message type
+            logging.error("Failed to receive RMR health check response")
+            return False
+
+        # If we reach here, the health check was successful
+        logging.info("RMR health check successful")
+        return True
