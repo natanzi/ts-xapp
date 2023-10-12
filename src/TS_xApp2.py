52cf7a7eb190dd44d99e5092264bd3c3dfd159ca
Milad Natanzi <miladnatanzi@gmail.com>
2023-10-12 16:48:10 -0400

    Update TS_xApp2.py

--- src/TS_xApp2.py
+++ src/TS_xApp2.py
@@ -5,11 +5,11 @@ import time
 import os
 from threading import Thread, Lock
 from ricxappframe.xapp_frame import RMRXapp, rmr, Xapp
-from menu import main_menu  # Importing the main_menu from menu.py
+from menu import main_menu
 from rmr_health_check import RMRHealthCheckXapp
 from sdl_health_check import sdl_health_check
 from alarm_handlers import handle_handover_failure, handle_data_retrieval_failure, handle_cell_congestion
-from path.to.SubscriptionHandler import SubscriptionHandler  # Adjust the import path as necessary
+from path.to.SubscriptionHandler import SubscriptionHandler  
 
 # Initialize logging
 logging.basicConfig(level=logging.INFO)
