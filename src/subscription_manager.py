110fa268d0c3f64a1bc88cd962c27aae5fb753d8
Milad Natanzi <miladnatanzi@gmail.com>
2023-10-12 17:28:40 -0400

    Update subscription_manager.py

--- src/subscription_manager.py
+++ src/subscription_manager.py
@@ -1,63 +1,31 @@
-"""
-Handles subscription messages from enbs and gnbs through rmr.
-"""
 import json
-from ricxappframe.xapp_frame import RMRXapp, rmr
-# Assuming _BaseHandler is defined in your project, adjust the import as necessary
-from ._BaseHandler import _BaseHandler  
+import ricxappframe.xapp_subscribe as subscribe
 
-class SubscriptionHandler(_BaseHandler):
+class SubscriptionManager:
+    def __init__(self, config, logger):
+        self.config = config
+        self.logger = logger
+        self.subscriber = None
+        self.subsCfgDetail = self.GetSubsConfig()
+        if self.subsCfgDetail:
+            self.subscriber = subscribe.NewSubscriber(self.subsCfgDetail['url'] + 'ric/v1')
 
-    def __init__(self, rmr_xapp: RMRXapp, msgtype):
-        super().__init__(rmr_xapp, msgtype)
+    def GetSubsConfig(self):
+        if self.config.cfg['controls'].get('subscription'):
+            return self.config.cfg['controls'].get('subscription')
 
-    def request_handler(self, rmr_xapp, summary, sbuf):
-        """
-        Handles subscription messages.
+    def Subscribe(self, subObj):
+        if self.subscriber:
+            self.logger.info("Sending the subscription to %s" % (self.subsCfgDetail['url'] + 'ric/v1'))
+            self.logger.info(subObj.to_dict())
+            data, reason, status = self.subscriber.Subscribe(subObj)
+            self.logger.info("Getting the subscription response")
+            self.logger.info(json.loads(data))
 
-        Parameters
-        ----------
-        rmr_xapp: rmr Instance Context
-        summary: dict (required)
-            buffer content
-        sbuf: str (required)
-            length of the message
-        """
-        self._rmr_xapp.rmr_free(sbuf)
-        try:
-            req = json.loads(summary[rmr.RMR_MS_PAYLOAD])  # input should be a json encoded as bytes
-            self.logger.debug("SubscriptionHandler.request_handler:: Handler processing request")
-        except (json.decoder.JSONDecodeError, KeyError):
-            self.logger.error("SubscriptionHandler.request_handler:: Handler failed to parse request")
-            return
+    def Unsubscribe(self, subscription_id):
+        if self.subscriber:
+            reason, status = self.subscriber.UnSubscribe(subscription_id)
 
-        if self.verifySubscription(req):
-            self.logger.info("SubscriptionHandler.request_handler:: Handler processed request: {}".format(req))
-        else:
-            self.logger.error("SubscriptionHandler.request_handler:: Request verification failed: {}".format(req))
-            return
-        self.logger.debug("SubscriptionHandler.request_handler:: Request verification success: {}".format(req))
-
-    def verifySubscription(self, req: dict):
-        """
-        Verifies the subscription request.
-
-        Parameters
-        ----------
-        req: dict
-            The subscription request to verify.
-
-        Returns
-        -------
-        bool
-            True if the subscription request is valid, False otherwise.
-        """
-        for i in ["subscription_id", "message"]:
-            if i not in req:
-                return False
-        return True
-
-    # Define additional methods as needed for TS xApp's subscription management
-    # For example, methods to subscribe, unsubscribe, handle notifications, etc.
-
-# End of SubscriptionHandler class
+    def QuerySubscriptions(self):
+        if self.subscriber:
+            data, reason, status = self.subscriber.QuerySubscriptions()
