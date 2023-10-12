3f0048634695b3286e1cba097b864f3db14d418a
Milad Natanzi <miladnatanzi@gmail.com>
2023-10-12 17:54:48 -0400

    Update TS_xApp2.py

--- src/TS_xApp2.py
+++ src/TS_xApp2.py
@@ -1,5 +1,4 @@
 # This is TS xApp version 1.0.0
-import logging
 import socket
 import time
 import os
@@ -12,9 +11,7 @@ from alarm_handlers import handle_handover_failure, handle_data_retrieval_failur
 from path.to.SubscriptionHandler import SubscriptionHandler
 from path.to.RMRManager import RMRManager
 from config import Config  # Importing the Config class
-
-# Initialize logging
-logging.basicConfig(level=logging.INFO)
+from logger import AppLogger  # Importing the AppLogger class
 
 def main():
     rmr_health_check = None
@@ -22,40 +19,48 @@ def main():
     rmr_manager = None  # RMR Manager instance
     subscription_handler = None  # Subscription handler instance
     config = None  # Configuration instance
+    app_logger = None  # Logger instance
 
     try:
         # Load the configuration
         config = Config(xapp_name="TS xApp", config_file="path/to/your/config.json")  # Replace with your actual xApp name and config file path
 
+        # Initialize the logger
+        app_logger = AppLogger(name="TS xApp", verbose=2)  # Replace with your actual xApp name and desired verbosity level
+        logger = app_logger.get_logger()
+
         # Initialize RMR health check
-        rmr_health_check = RMRHealthCheckXapp()
+        rmr_health_check = RMRHealthCheckXapp(logger)  # Pass the logger to the RMRHealthCheckXapp
         if not rmr_health_check.rmr_health_check():
-            logging.error("RMR health check failed. Exiting.")
+            logger.error("RMR health check failed. Exiting.")
             return
 
         # Perform SDL health check
         if not sdl_health_check():
-            logging.error("SDL health check failed. Exiting.")
+            logger.error("SDL health check failed. Exiting.")
             return
 
         # Initialize RMR Manager
-        rmr_manager = RMRManager(config)  # Pass the config to the RMRManager
+        rmr_manager = RMRManager(config, logger)  # Pass the config and logger to the RMRManager
         rmr_manager.start()  # Start RMR Manager
 
         # Initialize Subscription Handler
-        subscription_handler = SubscriptionHandler(config)  # Pass the config to the SubscriptionHandler
+        subscription_handler = SubscriptionHandler(config, logger)  # Pass the config and logger to the SubscriptionHandler
         subscription_handler.subscribe()  # Subscribe to necessary events
 
         # Start the interactive menu
         main_menu()
 
         # Initialize and run the Traffic Steering xApp
-        ts_xapp = TrafficSteering(config)  # Pass the config to the TrafficSteering xApp
+        ts_xapp = TrafficSteering(config, logger)  # Pass the config and logger to the TrafficSteering xApp
         ts_xapp.on_register()
         Thread(target=ts_xapp.run).start()
 
     except Exception as e:
-        logging.error(f"An error occurred: {e}")
+        if logger:
+            logger.error(f"An error occurred: {e}")
+        else:
+            print(f"An error occurred: {e}")  # Fallback to standard print if the logger is not initialized
 
     finally:
         if ts_xapp and ts_xapp.server:
@@ -67,7 +72,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-
-
-
