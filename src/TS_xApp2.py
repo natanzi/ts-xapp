c3b65a1378139a58dfd5d16d8a57cc59fb069c98
Milad Natanzi <miladnatanzi@gmail.com>
2023-10-12 15:43:45 -0400

    Update TS_xApp2.py

--- src/TS_xApp2.py
+++ src/TS_xApp2.py
@@ -10,7 +10,6 @@ from rmr_health_check import RMRHealthCheckXapp
 from sdl_health_check import sdl_health_check
 from alarm_handlers import handle_handover_failure, handle_data_retrieval_failure, handle_cell_congestion
 from path.to.SubscriptionHandler import SubscriptionHandler  # Adjust the import path as necessary
-from ricxappframe.xapp_frame import RMRXapp  # And other necessary imports
 
 
 # Initialize logging
