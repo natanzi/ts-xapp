e7887c306c6b44a44172ebc7e8ac4029c15ca496
Milad Natanzi <miladnatanzi@gmail.com>
2023-10-12 17:40:42 -0400

    Update TS_xApp2.py

--- src/TS_xApp2.py
+++ src/TS_xApp2.py
@@ -9,7 +9,8 @@ from menu import main_menu
 from rmr_health_check import RMRHealthCheckXapp
 from sdl_health_check import sdl_health_check
 from alarm_handlers import handle_handover_failure, handle_data_retrieval_failure, handle_cell_congestion
-from path.to.SubscriptionHandler import SubscriptionHandler  
+from path.to.SubscriptionHandler import SubscriptionHandler
+from path.to.RMRManager import RMRManager 
 
 # Initialize logging
 logging.basicConfig(level=logging.INFO)
@@ -17,6 +18,9 @@ logging.basicConfig(level=logging.INFO)
 def main():
     rmr_health_check = None
     ts_xapp = None
+    rmr_manager = None  # RMR Manager instance
+    subscription_handler = None  # Subscription handler instance
+
     try:
         # Initialize RMR health check
         rmr_health_check = RMRHealthCheckXapp()
@@ -28,6 +32,14 @@ def main():
         if not sdl_health_check():
             logging.error("SDL health check failed. Exiting.")
             return
+
+        # Initialize RMR Manager
+        rmr_manager = RMRManager()
+        rmr_manager.start()  # Start RMR Manager
+
+        # Initialize Subscription Handler
+        subscription_handler = SubscriptionHandler()
+        subscription_handler.subscribe()  # Subscribe to necessary events
 
         # Start the interactive menu
         main_menu()
@@ -43,8 +55,13 @@ def main():
     finally:
         if ts_xapp and ts_xapp.server:
             ts_xapp.server.close()
+        if rmr_manager:
+            rmr_manager.stop()  # Ensure RMR Manager is stopped properly
+        if subscription_handler:
+            subscription_handler.unsubscribe()  # Clean up subscriptions
 
 if __name__ == "__main__":
     main()
+
 
 
