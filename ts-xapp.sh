586a9e2e662c7e9aa18980229a1d86c950d898a6
Milad Natanzi <miladnatanzi@gmail.com>
2023-10-13 14:28:00 -0400

    Update ts-xapp.sh

--- ts-xapp.sh
+++ ts-xapp.sh
@@ -1,4 +1,5 @@
-apt install iperf3
+sudo apt install iperf3
+sudo apt install vim
 OAIC=`pwd`
 sudo apt-get install nginx
           
@@ -22,19 +23,19 @@ root /var/www/xApp_config.local/;
 echo ">>> reloading nginx..."
 sudo nginx -t 
 cd ${OAIC}
-sudo cp TS-xApp/TS-xApp-config-file.json /var/www/xApp_config.local/config_files/
-sudo chmod 755 /var/www/xApp_config.local/config_files/TS-xApp-config-file.json
+sudo cp ts-xApp/ts-xApp-config-file.json /var/www/xApp_config.local/config_files/
+sudo chmod 755 /var/www/xApp_config.local/config_files/ts-xApp-config-file.json
 sudo systemctl reload nginx
 echo ">>> getting machine IP..."
 export MACHINE_IP=`hostname  -I | cut -f1 -d' '`
 
 echo ">>> checking for config-file"
-curl http://${MACHINE_IP}:5010/config_files/TS-xApp-config-file.json
+curl http://${MACHINE_IP}:5010/config_files/ts-xApp-config-file.json
 echo ">>> building docker image...."
-cd ${OAIC}/TS-xApp
+cd ${OAIC}/ts-xApp
 echo ">>> checking directory"
 ls
-sudo docker build . -t xApp-registry.local:5008/TS-xApp:1.0.0
+sudo docker build . -t xApp-registry.local:5008/ts-xApp:1.0.0
 
 export KONG_PROXY=`sudo kubectl get svc -n ricplt -l app.kubernetes.io/name=kong -o jsonpath='{.items[0].spec.clusterIP}'`
 export APPMGR_HTTP=`sudo kubectl get svc -n ricplt --field-selector metadata.name=service-ricplt-appmgr-http -o jsonpath='{.items[0].spec.clusterIP}'`
@@ -47,16 +48,16 @@ echo "ONBOARDER_HTTP = $ONBOARDER_HTTP"
 echo ">>> getting charts..."
 curl --location --request GET "http://$KONG_PROXY:32080/onboard/api/v1/charts"
 ls
-echo '{"config-file.json_url":"http://'$MACHINE_IP':5010/config_files/TS-xApp-config-file.json"}' > TS-xApp-onboard.url
+echo '{"config-file.json_url":"http://'$MACHINE_IP':5010/config_files/ts-xApp-config-file.json"}' > ts-xApp-onboard.url
           
-echo ">>> TS-xApp-onboard.url"
-cat TS-xApp-onboard.url
+echo ">>> ts-xApp-onboard.url"
+cat ts-xApp-onboard.url
 echo ">>> curl POST..."
-curl -L -X POST "http://$KONG_PROXY:32080/onboard/api/v1/onboard/download" --header 'Content-Type: application/json' --data-binary "@TS-xApp-onboard.url"
+curl -L -X POST "http://$KONG_PROXY:32080/onboard/api/v1/onboard/download" --header 'Content-Type: application/json' --data-binary "@ts-xApp-onboard.url"
 
 echo ">>> curl GET..."
 curl -L -X GET "http://$KONG_PROXY:32080/onboard/api/v1/charts"
 echo ">>> curl POST..."
-curl -L -X POST "http://$KONG_PROXY:32080/appmgr/ric/v1/xapps" --header 'Content-Type: application/json' --data-raw '{"xappName": "TS-xApp"}'
+curl -L -X POST "http://$KONG_PROXY:32080/appmgr/ric/v1/xapps" --header 'Content-Type: application/json' --data-raw '{"xappName": "ts-xApp"}'
           
-echo 'Successful: TS-xApp up and running'
+echo 'Successful: ts-xApp up and running'
