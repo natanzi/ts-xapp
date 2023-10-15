520c76ab06c2e8562322a7b91415890e7ccdf51d
Milad Natanzi <miladnatanzi@gmail.com>
2023-10-14 20:24:20 -0400

    Update ts-xapp.sh

--- ts-xapp.sh
+++ ts-xapp.sh
@@ -1,3 +1,31 @@
+cat << 'EOF'
+################################################################################
+#          Welcome to OAIC Traffic Steering xApp Fast Installation             #
+#                                                                              #
+#   This script installs Nginx and configures the necessary files for running  #
+#   the Traffic Steering (TS) xApp.                                            #
+#                                                                              #
+#   The Traffic Steering (TS) xApp is a specialized tool designed for the      #
+#   OAIC O-RAN testbed. Its primary purpose is to efficiently manage and       #
+#   optimize traffic flow within cellular networks. By utilizing real-time     #
+#   metrics and adhering to dynamic policies, this xApp ensures optimal        #
+#   network performance by making informed decisions about user equipment      #
+#   (UE) handovers between cells. This xApp can be onboarded through the       #
+#   xApp Onboarder.                                                            #
+#                                                                              #
+#   Licensed under the Apache License, Version 2.0 (the "License");            #
+#   you may not use this file except in compliance with the License.           #
+#   You may obtain a copy of the License at:                                   #
+#       http://www.apache.org/licenses/LICENSE-2.0                             #
+#                                                                              #
+#   Unless required by applicable law or agreed to in writing, software        #
+#   distributed under the License is distributed on an "AS IS" BASIS,          #
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.   #
+#   See the License for the specific language governing permissions and        #
+#   limitations under the License.                                             #
+################################################################################
+EOF
+
 git clone https://github.com/natanzi/ts-xapp
 
 # Function to check if the script encounters any issues and prompts the user to continue or exit
