c5d35ccfb0814006eb74922d1b27eda1d55d970e
Milad Natanzi <miladnatanzi@gmail.com>
2023-10-20 15:00:50 -0400

    Update ts-xapp.py

--- src/ts-xapp.py
+++ src/ts-xapp.py
@@ -3,6 +3,9 @@ from menu import main_menu
 
 def main():
     print("xApp started. Loading main menu...")
+    print("xApp Onboarded and deployed successfully.")
+    print("xApp registered via E2.\n")
+    
     main_menu()  # This will run the main_menu function from menu.py
 
 if __name__ == "__main__":
