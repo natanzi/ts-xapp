14683910ef0848f46addc4152c3d52edd62c5569
Milad Natanzi <miladnatanzi@gmail.com>
2023-10-21 17:19:50 -0400

    Update ts-xapp.py

--- src/ts-xapp.py
+++ src/ts-xapp.py
@@ -5,13 +5,13 @@ import logging
 from flask import Flask, jsonify
 from ricxappframe.xapp_frame import RMRXapp, rmr, Xapp
 import subprocess
+import signal
 
 # Set up logging
 logging.basicConfig(filename='ts-xapp.log', level=logging.INFO,
                     format='%(asctime)s:%(levelname)s:%(message)s')
 
 app = Flask(__name__)
-
 
 @app.route('/rmr_health_check', methods=['POST'])
 def run_rmr_health_check():
@@ -23,7 +23,6 @@ def run_rmr_health_check():
         logging.error(f"Error executing RMR Health Check: {str(e)}")
         return jsonify(message=f"Error: {str(e)}"), 500
 
-
 @app.route('/sdl_health_check', methods=['POST'])
 def run_sdl_health_check():
     try:
@@ -33,7 +32,6 @@ def run_sdl_health_check():
     except Exception as e:
         logging.error(f"Error executing SDL Health Check: {str(e)}")
         return jsonify(message=f"Error: {str(e)}"), 500
-
 
 @app.route('/traffic_steering', methods=['POST'])
 def run_traffic_steering():
@@ -45,11 +43,19 @@ def run_traffic_steering():
         logging.error(f"Error executing Traffic Steering: {str(e)}")
         return jsonify(message=f"Error: {str(e)}"), 500
 
+# Store the subprocess reference
+dashboard_process = subprocess.Popen(["python3", "/app/src/dashboard.py"])
+
+# Define the signal handler function
+def terminate_process(signum, frame):
+    dashboard_process.terminate()
+    logging.info("Dashboard subprocess terminated.")
+    exit(0)
+
+# Attach the handler to SIGINT (Ctrl+C) and SIGTERM (termination signal)
+signal.signal(signal.SIGINT, terminate_process)
+signal.signal(signal.SIGTERM, terminate_process)
 
 if __name__ == "__main__":
-    # Start dashboard.py as a separate process
-    subprocess.Popen(["python3", "/path/to/dashboard.py"])
-    
     logging.info("ts-xApp starting...")
     app.run(host='0.0.0.0', port=8080)  # go to http://127.0.0.1:8080 and select the menu
-
