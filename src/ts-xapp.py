9635cd709c4832f7a613a0ca8f59cae04a212b70
Milad Natanzi <miladnatanzi@gmail.com>
2023-10-21 17:10:51 -0400

    Update ts-xapp.py

--- src/ts-xapp.py
+++ src/ts-xapp.py
@@ -4,22 +4,25 @@ import os
 import logging
 from flask import Flask, jsonify
 from ricxappframe.xapp_frame import RMRXapp, rmr, Xapp
+import subprocess
 
 # Set up logging
-logging.basicConfig(filename='ts-xapp.log', level=logging.INFO, 
+logging.basicConfig(filename='ts-xapp.log', level=logging.INFO,
                     format='%(asctime)s:%(levelname)s:%(message)s')
 
 app = Flask(__name__)
 
+
 @app.route('/rmr_health_check', methods=['POST'])
 def run_rmr_health_check():
     try:
-        rmr_health_check()  
+        rmr_health_check()
         logging.info("RMR Health Check executed")
         return jsonify(message="RMR Health Check executed")
     except Exception as e:
         logging.error(f"Error executing RMR Health Check: {str(e)}")
         return jsonify(message=f"Error: {str(e)}"), 500
+
 
 @app.route('/sdl_health_check', methods=['POST'])
 def run_sdl_health_check():
@@ -31,6 +34,7 @@ def run_sdl_health_check():
         logging.error(f"Error executing SDL Health Check: {str(e)}")
         return jsonify(message=f"Error: {str(e)}"), 500
 
+
 @app.route('/traffic_steering', methods=['POST'])
 def run_traffic_steering():
     try:
@@ -41,6 +45,11 @@ def run_traffic_steering():
         logging.error(f"Error executing Traffic Steering: {str(e)}")
         return jsonify(message=f"Error: {str(e)}"), 500
 
+
 if __name__ == "__main__":
+    # Start dashboard.py as a separate process
+    subprocess.Popen(["python3", "/path/to/dashboard.py"])
+    
     logging.info("ts-xApp starting...")
     app.run(host='0.0.0.0', port=8080)  # You can adjust the port as needed
+
