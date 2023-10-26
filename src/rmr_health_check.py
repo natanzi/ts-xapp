d25c178e8113614364cfd3cfc313710ac852d26f
Milad Natanzi <miladnatanzi@gmail.com>
2023-10-26 14:41:16 -0400

    Update rmr_health_check.py

--- src/rmr_health_check.py
+++ src/rmr_health_check.py
@@ -1,5 +1,17 @@
 # rmr_health_check.py
+from ricxappframe.xapp_frame import RMRXapp
+import logging
+
+def default_handler(summary, sbuf):
+    # You need to define the behavior of this handler
+    pass
+
+rmr_xapp = RMRXapp(default_handler, rmr_port=4560)
 
 def rmr_health_check():
-    print("Executing RMR Health Check...")
-    return "RMR Health Check completed successfully!"
+    logging.info("Executing RMR Health Check...")
+    ok = rmr_xapp.healthcheck()
+    if ok:
+        return "OK: RMR Health check completed successfully and it's fine"
+    else:
+        return "ERROR: RMR is unhealthy"
