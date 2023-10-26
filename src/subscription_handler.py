2b8430f4d326380d4a0d23b59de3d1a44858084f
Milad Natanzi <miladnatanzi@gmail.com>
2023-10-26 15:54:13 -0400

    Create subscription_handler.py

---
+++ src/subscription_handler.py
@@ -0,0 +1,7 @@
+#subscription_handler.py
+import logging
+
+def handle_subscription_response(summary, sbuf):
+    # Extract information from summary and sbuf
+    # Implement your logic for handling subscription responses here
+    logging.info("Subscription response received")
