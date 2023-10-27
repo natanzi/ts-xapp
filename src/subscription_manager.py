14e3cc841a4d65ce323751a125d62af3e26b3732
Milad Natanzi <miladnatanzi@gmail.com>
2023-10-27 18:12:42 -0400

    Update subscription_manager.py

--- src/subscription_manager.py
+++ src/subscription_manager.py
@@ -1,9 +1,10 @@
 # subscription_manager.py
 import logging
-from ricxappframe.xapp_subscribe import NewSubscriber, SubscriptionParams, SubscriptionDetail, ActionToBeSetup
+from ricxappframe.xapp_subscribe import NewSubscriber
+import ricxappframe.subsclient as subsclient
 
 class SubscriptionManager:
-    def __init__(self, uri="http://10.244.0.18:3800/", local_address="0.0.0.0", local_port=8088):
+    def __init__(self, uri, local_address="0.0.0.0", local_port=8088):
         self.subscriptions = {}
         self.subscriber = NewSubscriber(uri=uri, local_address=local_address, local_port=local_port)
         self.logger = logging.getLogger(__name__)
@@ -15,9 +16,10 @@ class SubscriptionManager:
 
     def send_subscription_request(self, subscription_id, payload):
         try:
-            action = ActionToBeSetup(action_id=1, action_type="report")
-            subscription_detail = SubscriptionDetail(xapp_event_instance_id=subscription_id, action_to_be_setup_list=[action])
-            params = SubscriptionParams(subscription_id=subscription_id, subscription_details=[subscription_detail])
+            action = subsclient.ActionToBeSetup(action_id=1, action_type="report")
+            subscription_detail = subsclient.SubscriptionDetail(xapp_event_instance_id=subscription_id, action_to_be_setup_list=[action])
+            params = subsclient.SubscriptionParams(subscription_id=subscription_id, subscription_details=[subscription_detail])
+            
             response, reason, status = self.subscriber.Subscribe(subs_params=params)
             if status == 200:
                 self.logger.info("Subscription request sent successfully for subscription ID: %s", subscription_id)
