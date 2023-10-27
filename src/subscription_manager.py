7e053409d1c0ef8539e0fbc03f82f71cbfb40e87
Milad Natanzi <miladnatanzi@gmail.com>
2023-10-27 15:42:26 -0400

    Update subscription_manager.py

--- src/subscription_manager.py
+++ src/subscription_manager.py
@@ -1,89 +1,46 @@
 #subscription_manager.py
 import logging
-import json
-from ricxappframe.xapp_frame import RMRXapp
+from ricxappframe.xapp_subscribe import NewSubscriber, SubscriptionParams, SubscriptionDetail, ActionToBeSetup
 
 class SubscriptionManager:
-    def __init__(self, rmr_xapp: RMRXapp):
-        self.rmr_xapp = rmr_xapp
-        self.subscriptions = {}  # A dictionary to store subscription information
+    def __init__(self, uri, local_address="0.0.0.0", local_port=8088):
+        self.subscriptions = {}
+        self.subscriber = NewSubscriber(uri=uri, local_address=local_address, local_port=local_port)
+        self.logger = logging.getLogger(__name__)
+        self.subscriber.ResponseHandler(responseCB=self.subscription_response_handler)
 
-    def get_gnb_list(self):
-        # TODO: Implement the logic to retrieve gNB IDs
-        # This is a placeholder list for demonstration
-        return ['gnb1', 'gnb2', 'gnb3']
-    
-    def get_enb_list(self):
-        # TODO: Implement the logic to retrieve eNB IDs
-        # This is a placeholder list for demonstration
-        return ['enb1', 'enb2', 'enb3']
-
-    def send_subscription_request(self, xnb_id):
-        subscription_id = xnb_id  # Using xnb_id as the subscription ID for simplicity
-        msg_type = 13010  # RIC Subscription Request message type
-        
-        # Creating a sample payload for the subscription request
-        payload = {
-            "subscription_id": subscription_id,
-            "xnb_id": xnb_id,
-            # Add other necessary fields here
-        }
-        
-        sbuf = self.rmr_xapp.rmr_alloc_msg(len(json.dumps(payload)))
-        sbuf.contents.mtype = msg_type
-        sbuf.contents.len = len(json.dumps(payload))
-        sbuf.contents.payload = json.dumps(payload).encode('utf-8')
-        self.rmr_xapp.rmr_send_msg(sbuf)
-        
-        logging.info("Subscription request sent for xnb ID: %s", xnb_id)
-        self.subscriptions[subscription_id] = {'status': 'pending', 'payload': payload}
-
-    def handle_subscription_response(self, summary, sbuf):
-        subscription_id = summary.get('subscription_id', 'Unknown')
-        if subscription_id in self.subscriptions:
-            self.subscriptions[subscription_id]['status'] = 'subscribed'
-            logging.info("Subscription successful for subscription ID: %s", subscription_id)
-        else:
-            logging.warning("Received subscription response for unknown subscription ID: %s", subscription_id)
-
-    def send_unsubscription_request(self, subscription_id):
-        if subscription_id not in self.subscriptions:
-            logging.warning("Cannot unsubscribe. Unknown subscription ID: %s", subscription_id)
-            return
-
-        msg_type = 13013  # RIC Subscription Delete Request message type
-        payload = self.subscriptions[subscription_id]['payload']
-        
-        sbuf = self.rmr_xapp.rmr_alloc_msg(len(json.dumps(payload)))
-        sbuf.contents.mtype = msg_type
-        sbuf.contents.len = len(json.dumps(payload))
-        sbuf.contents.payload = json.dumps(payload).encode('utf-8')
-        self.rmr_xapp.rmr_send_msg(sbuf)
-        
-        logging.info("Unsubscription request sent for subscription ID: %s", subscription_id)
-        self.subscriptions[subscription_id]['status'] = 'unsubscribing'
-
-    def handle_unsubscription_response(self, summary, sbuf):
-        subscription_id = summary.get('subscription_id', 'Unknown')
-        if subscription_id in self.subscriptions:
-            result = summary.get('result', 'fail')
-            if result == 'success':
-                del self.subscriptions[subscription_id]
-                logging.info("Unsubscription successful for subscription ID: %s", subscription_id)
+    def send_subscription_request(self, subscription_id, payload):
+        try:
+            action = ActionToBeSetup(action_id=1, action_type="report")
+            subscription_detail = SubscriptionDetail(xapp_event_instance_id=subscription_id, action_to_be_setup_list=[action])
+            params = SubscriptionParams(subscription_id=subscription_id, subscription_details=[subscription_detail])
+            
+            response, reason, status = self.subscriber.Subscribe(subs_params=params)
+            if status == 200:
+                self.logger.info("Subscription request sent successfully for subscription ID: %s", subscription_id)
+                self.subscriptions[subscription_id] = {'status': 'pending', 'payload': payload}
             else:
-                logging.error("Unsubscription failed for subscription ID: %s", subscription_id)
-                # Handle the error as needed
-        else:
-            logging.warning("Received unsubscription response for unknown subscription ID: %s", subscription_id)
+                self.logger.error("Failed to send subscription request for subscription ID: %s, Reason: %s, Status: %s", subscription_id, reason, status)
+        except Exception as e:
+            self.logger.error("An error occurred while sending subscription request for subscription ID: %s, Error: %s", subscription_id, str(e))
 
-    def initialize_subscriptions(self):
-        gnb_list = self.get_gnb_list()
-        enb_list = self.get_enb_list()
-        
-        for gnb in gnb_list:
-            self.send_subscription_request(gnb)
-            
-        for enb in enb_list:
-            self.send_subscription_request(enb)
-            
-        logging.info("Subscriptions initialized")
+    def subscription_response_handler(self, data):
+        try:
+            self.logger.info("Received subscription response: %s", data)
+            # Add logic to parse and handle the response data
+        except Exception as e:
+            self.logger.error("An error occurred while handling subscription response, Error: %s", str(e))
+
+    def unsubscribe(self, subscription_id):
+        try:
+            if subscription_id in self.subscriptions:
+                _, reason, status = self.subscriber.UnSubscribe(subs_id=str(subscription_id))
+                if status == 200:
+                    del self.subscriptions[subscription_id]
+                    self.logger.info("Unsubscription successful for subscription ID: %s", subscription_id)
+                else:
+                    self.logger.error("Failed to unsubscribe for subscription ID: %s, Reason: %s, Status: %s", subscription_id, reason, status)
+            else:
+                self.logger.warning("Unknown subscription ID: %s", subscription_id)
+        except Exception as e:
+            self.logger.error("An error occurred while unsubscribing for subscription ID: %s, Error: %s", subscription_id, str(e))
