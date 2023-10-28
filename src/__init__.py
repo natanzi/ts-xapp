42a3baadb94f3ca4b2cc7bdf4516acf56d2a6a9b
Milad Natanzi <miladnatanzi@gmail.com>
2023-10-28 18:52:40 -0400

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -4,6 +4,7 @@ import os
 import sys
 import subprocess
 import logging
+import signal
 
 logging.basicConfig(
     level=logging.INFO,
@@ -15,14 +16,24 @@ logging.basicConfig(
 )
 
 def load_config():
-    config_path = os.path.join(os.path.dirname(__file__), '../init/ts-xapp-config-file.json')
+    config_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '../init/ts-xapp-config-file.json'))
     try:
         with open(config_path, 'r') as f:
             config = json.load(f)
+            if not validate_config(config):
+                sys.exit(1)
             return config
     except Exception as e:
         logging.error(f"Error loading json file from {config_path}. Reason = {str(e)}")
         sys.exit(1)
+
+def validate_config(config):
+    required_keys = ["xapp_name", "version", "rmr"]
+    for key in required_keys:
+        if key not in config:
+            logging.error(f"Missing required key in config: {key}")
+            return False
+    return True
 
 def set_environment_variables(config):
     try:
@@ -37,13 +48,26 @@ def set_environment_variables(config):
 
 def start_ts_xapp():
     cmd = ["python3", "ts-xapp.py"]
-    subprocess.run(cmd)
+    process = subprocess.Popen(cmd)
+    return process
+
+def terminate_process(process):
+    if process:
+        process.terminate()
+        logging.info("ts-xapp.py terminated.")
+    sys.exit(0)
 
 if __name__ == "__main__":
     config = load_config()
     
     if set_environment_variables(config):
         logging.info("Environment variables set successfully")
-        start_ts_xapp()
+        process = start_ts_xapp()
+        
+        # Attach the handler to SIGINT (Ctrl+C) and SIGTERM (termination signal)
+        signal.signal(signal.SIGINT, lambda signum, frame: terminate_process(process))
+        signal.signal(signal.SIGTERM, lambda signum, frame: terminate_process(process))
+        
+        process.wait()
     else:
         logging.error("Failed to set environment variables")
