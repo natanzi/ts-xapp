e6f873143d423d21e6b80a41148f6b01bcc587eb
Milad Natanzi <miladnatanzi@gmail.com>
2023-10-28 18:13:50 -0400

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,17 +1,50 @@
-# ==================================================================================
-#
-#       Copyright (c) 2020 Samsung Electronics Co., Ltd. All Rights Reserved.
-#
-#   Licensed under the Apache License, Version 2.0 (the "License");
-#   you may not use this file except in compliance with the License.
-#   You may obtain a copy of the License at
-#
-#          http://www.apache.org/licenses/LICENSE-2.0
-#
-#   Unless required by applicable law or agreed to in writing, software
-#   distributed under the License is distributed on an "AS IS" BASIS,
-#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#   See the License for the specific language governing permissions and
-#   limitations under the License.
-#
-# ==================================================================================
+#__init__.py
+import json
+import os
+import sys
+import subprocess
+import signal
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s:%(levelname)s:%(message)s',
+    handlers=[
+        logging.FileHandler('init-ts-xapp.log'),
+        logging.StreamHandler(sys.stdout)
+    ]
+)
+
+def load_config(config_path):
+    try:
+        with open(config_path, 'r') as f:
+            config = json.load(f)
+            return config
+    except Exception as e:
+        logging.error(f"Error loading json file from {config_path}. Reason = {str(e)}")
+        sys.exit(1)
+
+def set_environment_variables(config):
+    try:
+        os.environ["XAPP_NAME"] = config["xapp_name"]
+        os.environ["VERSION"] = config["version"]
+        os.environ["RMR_PORT"] = str(config["rmr"]["protPort"])
+        # Add any other environment variables you need to set here
+        return True
+    except Exception as e:
+        logging.error(f"Error setting environment variables. Reason = {str(e)}")
+        return False
+
+def start_ts_xapp():
+    cmd = ["python3", "ts-xapp.py"]
+    subprocess.run(cmd)
+
+if __name__ == "__main__":
+    config_file = os.getenv("CONFIG_FILE", "init/ts-xapp-config-file.json")
+    config = load_config(config_file)
+    
+    if set_environment_variables(config):
+        logging.info("Environment variables set successfully")
+        start_ts_xapp()
+    else:
+        logging.error("Failed to set environment variables")
