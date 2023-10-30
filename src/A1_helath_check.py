c3095deba697e933b599d99b16843a41fb0c9dde
Milad Natanzi <miladnatanzi@gmail.com>
2023-10-30 17:17:22 -0400

    Update A1_helath_check.py

--- src/A1_helath_check.py
+++ src/A1_helath_check.py
@@ -3,3 +3,22 @@
 #When the xApp starts, the A1PolicyManager sends an A1 policy query to request the current policy.When an A1 policy request is received, the A1PolicyHandler verifies the request and builds a response.
 #The response is then sent back over the A1 interface using the rmr_send method of the RMRXapp instance.This interaction allows the xApp to manage and respond to A1 policies, demonstrating how an xApp can interact with the A1 interface.
 #####################################################################################################################################################################
+import logging
+from ricxappframe.xapp_frame import RMRXapp
+from constants import Constants
+
+class A1HealthCheck:
+    def __init__(self, rmr_xapp: RMRXapp):
+        self._rmr_xapp = rmr_xapp
+        self.logger = logging.getLogger(__name__)
+
+    def perform_health_check(self):
+        try:
+            # Here, you would send a health check message over the A1 interface and wait for a response
+            # This is a simplified example and should be replaced with actual health check logic
+            self._rmr_xapp.rmr_send(b'Health check', Constants.A1_HEALTH_CHECK_REQ)
+            self.logger.info("A1 health check request sent")
+            return "A1 interface is healthy"
+        except Exception as e:
+            self.logger.error(f"A1 health check failed: {str(e)}")
+            return "A1 interface is not responding"
