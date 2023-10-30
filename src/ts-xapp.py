942c67a2331781755cac97242009de14b600db6c
Milad Natanzi <miladnatanzi@gmail.com>
2023-10-30 19:05:25 -0400

    Update ts-xapp.py

--- src/ts-xapp.py
+++ src/ts-xapp.py
@@ -17,7 +17,7 @@ from e2_health_check import e2_health_check
 from traffic_steering import traffic_steering
 from data_sync import sync_kpimon_data
 from .handler.A1PolicyHandler import A1PolicyHandler
-from src.A1PolicyHandler import A1PolicyHandler
+from src.handler.A1PolicyHandler import A1PolicyHandler
 from manager.A1PolicyManager import A1PolicyManager
 from A1_health_check import A1HealthCheck
 from constants import Constants
