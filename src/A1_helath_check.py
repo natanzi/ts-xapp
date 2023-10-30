232f8d994c9c5a6ccf052e70332a8d07d1efea9b
Milad Natanzi <miladnatanzi@gmail.com>
2023-10-30 16:14:01 -0400

    Create A1_helath_check.py

---
+++ src/A1_helath_check.py
@@ -0,0 +1,5 @@
+#A1_helath_check.py
+#####################################################################################################################################################################
+#When the xApp starts, the A1PolicyManager sends an A1 policy query to request the current policy.When an A1 policy request is received, the A1PolicyHandler verifies the request and builds a response.
+#The response is then sent back over the A1 interface using the rmr_send method of the RMRXapp instance.This interaction allows the xApp to manage and respond to A1 policies, demonstrating how an xApp can interact with the A1 interface.
+#####################################################################################################################################################################
