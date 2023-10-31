9afaf31eed02ffddb10553935d44c1d683f1cc1b
Milad Natanzi <miladnatanzi@gmail.com>
2023-10-31 11:46:22 -0400

    Update A1PolicyHandler.py

--- src/A1PolicyHandler.py
+++ src/A1PolicyHandler.py
@@ -9,25 +9,25 @@ class A1PolicyHandler:
         self.logger = logging.getLogger(__name__)
 
     def request_handler(self, summary, sbuf):
-        self._rmr_xapp.rmr_free(sbuf)
-        try:
-            req = json.loads(summary['payload'])  # input should be a json encoded as bytes
-            self.logger.debug("A1PolicyHandler.request_handler:: Handler processing request")
-        except (json.decoder.JSONDecodeError, KeyError):
-            self.logger.error("A1PolicyHandler.request_handler:: Handler failed to parse request")
-            return
+    self._rmr_xapp.rmr_free(sbuf)
+    try:
+        req = json.loads(summary['payload'])  # input should be a json encoded as bytes
+        self.logger.debug("A1PolicyHandler.request_handler:: Handler processing request")
+    except (json.decoder.JSONDecodeError, KeyError):
+        self.logger.error("A1PolicyHandler.request_handler:: Handler failed to parse request")
+        return
 
-        if self.verifyPolicy(req):
-            self.logger.info("A1PolicyHandler.request_handler:: Handler processed request: {}".format(req))
-        else:
-            self.logger.error("A1PolicyHandler.request_handler:: Request verification failed: {}".format(req))
-            return
-        self.logger.debug("A1PolicyHandler.request_handler:: Request verification success: {}".format(req))
+    if self.verifyPolicy(req):
+        self.logger.info("A1PolicyHandler.request_handler:: Handler processed request: {}".format(req))
+    else:
+        self.logger.error("A1PolicyHandler.request_handler:: Request verification failed: {}".format(req))
+        return
+    self.logger.debug("A1PolicyHandler.request_handler:: Request verification success: {}".format(req))
 
-        resp = self.buildPolicyResp(req)
-        self._rmr_xapp.rmr_send(json.dumps(resp).encode(), Constants.A1_POLICY_RESP)
-        self.logger.info("A1PolicyHandler.request_handler:: Response sent: {}".format(resp))
-
+    resp = self.buildPolicyResp(req)
+    self._rmr_xapp.rmr_send(json.dumps(resp).encode(), Constants.A1_POLICY_RESP)
+    self.logger.info("A1PolicyHandler.request_handler:: Response sent: {}".format(resp))
+    
     def verifyPolicy(self, req: dict):
         for i in ["policy_type_id", "operation", "policy_instance_id"]:
             if i not in req:
