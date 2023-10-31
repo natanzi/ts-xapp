79d77239f88a4592580c0a9a9c67cad5537f8db1
Milad Natanzi <miladnatanzi@gmail.com>
2023-10-31 12:21:42 -0400

    Update and rename A1_helath_check.py to A1_health_check.py

--- src/A1_health_check.py
+++ src/A1_health_check.py
@@ -1,11 +1,11 @@
-#A1_helath_check.py
+#A1_health_check.py
 #####################################################################################################################################################################
 #When the xApp starts, the A1PolicyManager sends an A1 policy query to request the current policy.When an A1 policy request is received, the A1PolicyHandler verifies the request and builds a response.
 #The response is then sent back over the A1 interface using the rmr_send method of the RMRXapp instance.This interaction allows the xApp to manage and respond to A1 policies, demonstrating how an xApp can interact with the A1 interface.
 #####################################################################################################################################################################
 import logging
 from ricxappframe.xapp_frame import RMRXapp
-A1_helath_check import A1HealthCheck
+from constants import Constants
 
 class A1HealthCheck:
     def __init__(self, rmr_xapp: RMRXapp):
