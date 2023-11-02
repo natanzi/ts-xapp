484745b966a53de8eed3c50cd03fd9b4b3888f4a
Milad Natanzi <miladnatanzi@gmail.com>
2023-11-02 17:39:21 -0400

    Update handover_client.py

--- handover_client.py
+++ handover_client.py
@@ -28,10 +28,26 @@ def send_handover_command(server_address, ue_id, target_cell):
             # Wait for a response
             response = s.recv(1024)
             print('Received:', response.decode('utf-8'))
+
+        except socket.error as e:
+            print(f"Socket error occurred: {e}")
+
+        except json.JSONDecodeError as e:
+            print(f"JSON decode error: {e}")
+
         except Exception as e:
             print(f"An error occurred: {e}")
+
+        finally:
+            # Attempt to close the socket in case of any failure
+            try:
+                s.shutdown(socket.SHUT_RDWR)
+            except Exception:
+                pass  # Ignore errors in shutdown
+            s.close()
 
 # Example usage
 if __name__ == "__main__":
     # Replace 'ue_id' and 'target_cell' with appropriate values
     send_handover_command((HOST, PORT), ue_id='1234', target_cell='5678')
+
