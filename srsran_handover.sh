7eade2390f1582ba3d79c5782f5a98dcb0dd742d
Milad Natanzi <miladnatanzi@gmail.com>
2023-11-03 17:12:30 -0400

    Update and rename enable_handover.sh to srsran_handover.sh

--- srsran_handover.sh
+++ srsran_handover.sh
@@ -12,29 +12,23 @@ if [ "$(pwd)" != "$REQUIRED_DIR" ]; then
   exit 1
 fi
 
-# Function to compare file checksums
-compare_checksums() {
-  local src_file=$1
-  local dest_file=$2
+######################### Install srsRAN #####################
+sudo apt-get install build-essential cmake libfftw3-dev libmbedtls-dev libboost-program-options-dev libconfig++-dev libsctp-dev
+sudo apt-get install libzmq3-dev
+sudo add-apt-repository ppa:ettusresearch/uhd
+sudo apt-get update
+sudo apt-get install libuhd-dev libuhd4.1.0 uhd-host
+git clone https://gitlab.eurecom.fr/oai/asn1c.git
+cd asn1c
+git checkout velichkov_s1ap_plus_option_group
+autoreconf -iv
+./configure
+cd ..
 
-  # Calculate MD5 checksums
-  local src_checksum=$(md5sum "$src_file" | cut -d ' ' -f1)
-  local dest_checksum=$(md5sum "$dest_file" | cut -d ' ' -f1)
-
-  # Compare the checksums
-  if [ "$src_checksum" == "$dest_checksum" ]; then
-    echo "Checksums match for $src_file and $dest_file."
-    return 0
-  else
-    echo "Checksums do not match for $src_file and $dest_file!" >&2
-    return 1
-  fi
-}
-
-# Define the destination directory
+# Define the destination directory where srsRAN is located
 DEST_DIR="/home/ubnt/main-file-repo/oaic/srsRAN-e2"
 
-# Define the URLs of the source files
+# Define the URLs of the source files to be replaced
 declare -A files_to_download=(
   ["srsenb/src/main.cc"]="https://raw.githubusercontent.com/natanzi/srsRAN_4G_handover/master/srsenb/src/main.cc"
   ["lib/include/srsran/common/handover_server.h"]="https://raw.githubusercontent.com/natanzi/srsRAN_4G_handover/master/lib/include/srsran/common/handover_server.h"
@@ -53,15 +47,33 @@ for dest_path in "${!files_to_download[@]}"; do
   # Download the file using wget or curl
   if wget -q -O "$full_dest_path" "$url" || curl -s -o "$full_dest_path" "$url"; then
     echo "Downloaded $url to $full_dest_path"
-    # Verify the download by comparing checksums with the online file
-    if ! compare_checksums <(curl -s "$url") "$full_dest_path"; then
-      echo "Download verification failed for $url to $full_dest_path" >&2
-      exit 1
-    fi
   else
     echo "Failed to download $url to $full_dest_path" >&2
     exit 1
   fi
 done
 
-echo "All files have been downloaded and verified successfully."
+echo "Files have been downloaded successfully."
+
+# Navigate to the srsRAN build directory and build the project
+cd "${DEST_DIR}"
+if [ ! -d "build" ]; then
+  mkdir build
+fi
+cd build
+cmake ..
+make -j$(nproc)
+sudo make install
+sudo ldconfig
+
+# Export the environment variable for srsRAN
+export SRS=`realpath .`
+# Additional cmake configurations, assuming e2_bindings directory exists under ${SRS}
+cmake ../ -DCMAKE_BUILD_TYPE=RelWithDebInfo \
+          -DRIC_GENERATED_E2AP_BINDING_DIR=${SRS}/e2_bindings/E2AP-v01.01 \
+          -DRIC_GENERATED_E2SM_KPM_BINDING_DIR=${SRS}/e2_bindings/E2SM-KPM \
+          -DRIC_GENERATED_E2SM_GNB_NRT_BINDING_DIR=${SRS}/e2_bindings/E2SM-GNB-NRT
+make -j$(nproc)
+sudo make install
+sudo ldconfig
+sudo srsran_install_configs.sh service
