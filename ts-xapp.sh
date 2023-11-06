24020236bd155f2287785fe570e610a9f5e5da0f
Milad Natanzi <miladnatanzi@gmail.com>
2023-11-06 17:17:40 -0500

    Update ts-xapp.sh

--- ts-xapp.sh
+++ ts-xapp.sh
@@ -73,6 +73,7 @@ echo "Installing vim..."
 sudo apt install vim || { echo 'vim installation failed'; check_continue; }
 echo "vim installation completed."
 apt install python3-pip
+sudo apt install nlohmann-json3-dev
 
 
 oaic=`pwd`
